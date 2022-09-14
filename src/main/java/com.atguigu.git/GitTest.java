754302befa9967dbad0aa173424dfe68291a3bda
ljy <ljy@qq.com>
2022-09-14 21:14:23 +0800

    2次提交

--- src/main/java/com.atguigu.git/GitTest.java
+++ src/main/java/com.atguigu.git/GitTest.java
@@ -2,6 +2,6 @@ package src.main.java.com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello1");
     }
 }
