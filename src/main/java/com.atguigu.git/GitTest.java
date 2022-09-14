ab323ba957458ead1e4d3907535785ce8f176df3
ljy <ljy@qq.com>
2022-09-14 21:31:08 +0800

    hot-fix提交1

--- src/main/java/com.atguigu.git/GitTest.java
+++ src/main/java/com.atguigu.git/GitTest.java
@@ -3,5 +3,7 @@ package src.main.java.com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
